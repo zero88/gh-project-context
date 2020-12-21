4a3b75e2021201188b7c17a3356488c974f577ef
zero88 <sontt246@gmail.com>
2020-12-21 18:23:27 +0700

    First step

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  printWidth: 120,
+  quoteProps: 'consistent',
+};
